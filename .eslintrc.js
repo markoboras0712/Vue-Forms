dbd64088ba8993d0720aa7f8e5bdfb1b3a072c40
markoboras0712 <mboras1@etfos.hr>
2021-04-27 13:43:01 +0300

    submitting forms

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
   root: true,
   env: {
     node: true
@@ -12,6 +12,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    quotes: [0, "double"]
   }
 }
+*/
--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Create an event</h1>
-    <form>
+    <form @submit.prevent="sendForm">
       <BaseSelect
         :options="categories"
         v-model="event.category"
@@ -10,25 +10,13 @@
 
       <h3>Name & describe your event</h3>
 
-      <BaseInput
-        v-model="event.title"
-        label="Title"
-        type="text"
-      />
+      <BaseInput v-model="event.title" label="Title" type="text" />
 
-      <BaseInput
-        v-model="event.description"
-        label="Description"
-        type="text"
-      />
+      <BaseInput v-model="event.description" label="Description" type="text" />
 
       <h3>Where is your event?</h3>
 
-      <BaseInput
-        v-model="event.location"
-        label="Location"
-        type="text"
-      />
+      <BaseInput v-model="event.location" label="Location" type="text" />
 
       <h3>Are pets allowed?</h3>
       <div>
@@ -41,17 +29,11 @@
 
       <h3>Extras</h3>
       <div>
-        <BaseCheckbox
-          v-model="event.extras.catering"
-          label="Catering"
-        />
+        <BaseCheckbox v-model="event.extras.catering" label="Catering" />
       </div>
 
       <div>
-        <BaseCheckbox
-          v-model="event.extras.music"
-          label="Live music"
-        />
+        <BaseCheckbox v-model="event.extras.music" label="Live music" />
       </div>
 
       <button type="submit">Submit</button>
@@ -62,23 +44,25 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  data () {
+  data() {
     return {
       categories: [
-        'sustainability',
-        'nature',
-        'animal welfare',
-        'housing',
-        'education',
-        'food',
-        'community'
+        "sustainability",
+        "nature",
+        "animal welfare",
+        "housing",
+        "education",
+        "food",
+        "community"
       ],
       event: {
-        category: '',
-        title: '',
-        description: '',
-        location: '',
+        category: "",
+        title: "",
+        description: "",
+        location: "",
         pets: 1,
         extras: {
           catering: false,
@@ -86,10 +70,24 @@ export default {
         }
       },
       petOptions: [
-        { label: 'Yes', value: 1 },
-        { label: 'No', value: 0 }
+        { label: "Yes", value: 1 },
+        { label: "No", value: 0 }
       ]
+    };
+  },
+  methods: {
+    sendForm(e) {
+      axios.post(
+          "https://my-json-server.typicode.com/Code-Pop/Vue-3-Forms/events",
+          this.event
+        )
+        .then(function(response) {
+          console.log("Response", response);
+        })
+        .catch(function(err) {
+          console.log("Error", err);
+        });
     }
   }
-}
+};
 </script>
